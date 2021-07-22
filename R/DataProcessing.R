a056cbc29a856b0bcc2471be1b68087071d431c6
Brian Helsel <bhelsel80@gmail.com>
2021-07-22 11:53:54 -0500

    Added new functions
    Added new functions that I've been using frequently, which includes a gt3x2csv converter for single files and a function to convert dummy variables back into categorical.

---
+++ R/DataProcessing.R
@@ -0,0 +1,69 @@
+#' @title Mean and Standard Deviation
+#' @description Prints out mean and standard deviation for a continuous variable.
+#' @param x Name of a continuous variable in data.
+#' @return Mean and Standard Deviation.
+#' @details Prints out mean and standard deviation for a continuous variable.
+#' @examples 
+#' \dontrun{
+#'  x = rnorm(100, 40, 5)
+#'  meansd(x)
+#'  }
+#' @rdname meansd
+#' @export 
+
+x = rnorm(100, 40, 5)
+meansd(x)
+meansd <- function(x) {
+  paste0("Mean: ", round(mean(x, na.rm=TRUE), 2), "; ", "SD: ", round(sd(x, na.rm=TRUE), 2))
+}
+
+#' @title Frequency and Percentage
+#' @description Prints out the frequency and percentage for a categorical or binary variable.
+#' @param x Name of a categorical or binary variable in data.
+#' @param total Number of total observations for the categorical or binary variable.
+#' @return Frequency and percentage of the categorical or binary variable.
+#' @details Prints out the frequency and percentage for a categorical or binary variable.
+#' @examples 
+#' \dontrun{
+#' #EXAMPLE1
+#' x = rnorm(100, 40, 5)
+#' y = ifelse(x < 40, 1, 0)
+#' freqper(y, 100)
+#'  }
+#' @rdname freqper
+#' @export 
+
+freqper <- function(x, total){
+  x_table <- as.matrix(table(x))
+  x_table <- cbind(x_table, round(((table(x)/total)*100), 2))
+  cat(paste0(rownames(x_table), ": Frequency: ", x_table[, 1], "; ", "Percentage ", x_table[, 2], "%", "\n"))
+}
+
+#' @title Dummy 2 Categorical
+#' @description Converts several related dummy variables to a single categorical variable.
+#' @param data Data set where the variables can be located.
+#' @param id The unique identifier for the participants in the data.
+#' @param new_var The name of the new categorical variable that will be created.
+#' @param column_list Combined list using c() of the data columns containing the dummy variables.
+#' @param new_names The new categories that will be created within the categorical variable.
+#' @return Returns the data set with the new categorical variable.
+#' @details Converts several related dummy variables to a single categorical variable.
+#' @examples 
+#' \dontrun{
+#'  #Add examples
+#' }
+#' @rdname dummy_2_categorical
+#' @export 
+
+dummy_2_categorical <- function(data, id, new_var, column_list, new_names){
+  subset <- data[, c(id, column_list)]
+  colnames(subset) <- c(id, new_names)
+  subset[, new_var] <- names(subset[new_names])[max.col(subset[new_names])]
+  subset[, new_names] <- NULL
+  data <- merge(data, subset, by=id)
+  return(data)
+}
+
+df <- dummy_2_categorical(data = df, id="id", new_var = "diagnosis",
+                          c("secondary_diagnosis___1", "secondary_diagnosis___2", "secondary_diagnosis___3"),
+                          c("Autism", "Down_Syndrome", "Other_IDD"))
---
+++ R/gt3x_to_csv.R
@@ -0,0 +1,43 @@
+#' @title gt3x2csv
+#' @description Convert a single GT3X file to a CSV file with removed timestamp to be processed by GGIR.
+#' @param origin Origin of the GT3X file.
+#' @param output Desired location of the CSV file.
+#' @param studyname Study name or abbreviation to be used in the naming of the file.
+#' @return Returns a CSV file to the path name specified under output.
+#' @details Convert a single GT3X file to a CSV file with removed timestamp to be processed by GGIR.
+#' @examples 
+#' \dontrun{
+#'  #Add example here.
+#' }
+#' @rdname gt3x2csv
+#' @export 
+
+library(read.gt3x)
+library(readr)
+directory = "/Volumes/data/Shared/Weight_Management/Public/Brian Helsel/6. Data Analysis/BP Max Test"
+filename = "A1401 (2020-02-07).gt3x"
+origin <- paste0(directory, "/", filename)
+output <- paste0(directory, "/", "GGIR Data")
+studyname <- "BrainPower"
+
+gt3x2csv <- function(origin, output, studyname){
+  info <- parse_gt3x_info(origin)
+  df <- read.gt3x(origin, asDataFrame = TRUE, imputeZeroes = TRUE)
+  df$time <- NULL
+  id <- info$`Subject Name`
+  colnames(df) <- c("Accelerometer X", "Accelerometer Y", "Accelerometer Z")
+  header1 <- "------------ Data File Created By ActiGraph GT3X+ ActiLife v6.13.4 Firmware v3.2.1 date format yyyy.MM.dd at 60 Hz  Filter Normal -----------"
+  header2 <- paste0("Serial Number: ", info$`Serial Number`)
+  header3 <- paste0("Start Time ", format(info$`Start Date`, "%H:%M:%S"))
+  header4 <- paste0("Start Date ", format(info$`Start Date`, "%Y.%m.%d"))
+  header5 <- "Epoch Period (hh:mm:ss) 00:00:00"
+  header6 <- paste0("Download Time ", format(info$`Download Date`, "%H:%M:%S"))
+  header7 <- paste0("Download Date ", format(info$`Download Date`, "%Y.%m.%d"))
+  header8 <- "Current Memory Address: 0"
+  header9 <- paste0("Current Battery Voltage: ", info$`Battery Voltage`,"     Mode = 12")
+  header10 <- "--------------------------------------------------"
+  header_txt <- data.frame(header = rbind(header1, header2, header3, header4, header5, header6, header7, header8, header9, header10))
+  write_csv(header_txt, paste0(output, "/", id, "_", studyname, ".csv"), append = FALSE, col_names = FALSE)
+  write_csv(df, paste0(output, "/", id, "_", studyname, ".csv"), append = TRUE, col_names = TRUE)
+}
+
