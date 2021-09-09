f0472756239a71eca414f545eacc899c24abb652
Brian Helsel <bhelsel80@gmail.com>
2021-09-09 12:28:15 -0500

    Added package description

---
+++ R/bhelselR.R
@@ -0,0 +1,18 @@
+#' bhelselR: A package for computating the notorious bar statistic
+#'
+#' The bhelselR package provides several important functions:
+#' 
+#' @section Foo functions:
+#' agd_to_csv
+#' child_bmi_percentile
+#' dummy_2_categorical
+#' freqper
+#' gt3x2csv
+#' meansd
+#' percent_over_bmi_50
+#' read_agd
+#'
+#' @docType package
+#' @name bhelselR
+NULL
+#> NULL
