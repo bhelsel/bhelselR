bfc1c6b6689a902a249c0e212fd227db1082bc85
Brian Helsel <bhelsel80@gmail.com>
2021-09-09 13:52:51 -0500

    Updated Package Documentation

--- R/bhelselR.R
+++ R/bhelselR.R
@@ -3,17 +3,15 @@
 #' @description The bhelselR package provides several important functions:
 #' 
 #' @section bhelselR functions:
-#' * [bhelselR::readActigraph()]
-#' * child_bmi_percentile
-#' * dummy_2_categorical
-#' * freqper
-#' * gt3x2csv
-#' * meansd
-#' * percent_over_bmi_50
-#' * read_agd
+#' * [child_bmi_percentile][bhelselR::child_bmi_percentile()]
+#' * [dummy_2_categorical][bhelselR::dummy_2_categorical()]
+#' * [freqper][bhelselR::freqper()]
+#' * [gt3x2csv][bhelselR::gt3x2csv()]
+#' * [meansd][bhelselR::meansd()]
+#' * [percent_over_bmi_50][bhelselR::percent_over_bmi_50()]
+#' * [read_agd][bhelselR::read_agd()]
 #'
 #' @docType package
 #' @name bhelselR
 NULL
 #> NULL
-
