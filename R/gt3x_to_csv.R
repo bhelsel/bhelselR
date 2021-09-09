e8206d730e30058aaee842762c93e458e50a532d
Brian Helsel <bhelsel80@gmail.com>
2021-09-09 10:24:46 -0500

    Added read_agd and agd_to_csv functions
    Added read_agd and agd_to_csv functions

--- R/gt3x_to_csv.R
+++ R/gt3x_to_csv.R
@@ -32,3 +32,112 @@ gt3x2csv <- function(origin, output, studyname){
   data.table::fwrite(header_txt, paste0(output, "/", id, "_", studyname, ".csv"), append = FALSE, col.names = FALSE)
   data.table::fwrite(df, paste0(output, "/", id, "_", studyname, ".csv"), append = TRUE, col.names = TRUE)
 }
+
+#' @title Read ActiGraph AGD Files
+#' @description Read the settings or data from the ActiGraph AGD Files
+#' @param agd_filename The path to the AGD file or the filename if the AGD file is in the current working directory.
+#' @param settings Returns the AGD file meta data that is reported in ActiGraph CSV Files in a data frame, Default: FALSE
+#' @param data Returns the AGD file accelerometer data in a data frame, Default: TRUE
+#' @return Returns a data frame or a list if settings and data are both TRUE
+#' @details Read the settings or data from the ActiGraph AGD Files
+#' @examples 
+#' \dontrun{
+#' if(interactive()){
+#'  #EXAMPLE1
+#'  }
+#' }
+#' @seealso 
+#'  \code{\link[DBI]{dbConnect}},\code{\link[DBI]{dbReadTable}},\code{\link[DBI]{dbDisconnect}}
+#'  \code{\link[RSQLite]{SQLite}}
+#' @rdname read_agd
+#' @export 
+#' @importFrom DBI dbConnect dbReadTable dbDisconnect
+#' @importFrom RSQLite SQLite
+read_agd <- function(agd_filename, settings=FALSE, data=TRUE) {
+  con <- DBI::dbConnect(RSQLite::SQLite(), agd_filename)
+  if (settings==TRUE){
+    agd_set <- DBI::dbReadTable(con, "settings")
+    get_value <- function(settingName) {agd_set[agd_set["settingName"]==settingName, "settingValue"]}
+    get_time <- function(settingName) {as.POSIXlt((as.numeric(agd_set[agd_set["settingName"]==settingName, "settingValue"]) / 1e7), origin = "0001-01-01 00:00:00", tz = "GMT")}
+    start_datetime <- get_time("startdatetime")
+    start_date <- format(start_datetime, "%m-%d-%Y")
+    start_time <- strftime(start_datetime, format = "%H:%M:%S")
+    download_datetime = get_time("downloaddatetime")
+    download_date <- format(download_datetime, "%m-%d-%Y")
+    download_time <- strftime(download_datetime, format = "%H:%M:%S")
+    epochlength <- strftime(as.POSIXlt(as.numeric(get_value("epochlength")), origin = "0001-01-01 00:00:00", tz="GMT"), format = "%H:%M:%S")
+    r1 = paste0("------------ Data Table File Created By Actigraph", " ", get_value("devicename"), " ", get_value("softwarename"), " v", get_value("softwareversion"), " ", "date format", " ", get_value("datetimeformat"), " ", "Filter", " ", get_value("filter"), " -----------")
+    r2 = paste0("Serial Number:", " ", get_value("deviceserial"))
+    r3 = paste0("Start Time", " ", start_time)
+    r4 = paste0("Start Date", " ", start_date)
+    r5 = paste0("Epoch Length (hh:mm:ss)", " ", epochlength)
+    r6 = paste0("Download Time", " ", download_time)
+    r7 = paste0("Download Date", " ", download_date)
+    r8 = paste0("Current Memory Address:", " ", "0")
+    r9 = paste0("Current Battery Voltage:", " ", get_value("batteryvoltage"), "     ", "Mode", " = ", get_value("modenumber"))
+    r10 = paste0("--------------------------------------------------")
+    agd_set = data.frame(rbind(r1, r2, r3, r4, r5, r6, r7, r8, r9, r10))
+  }
+  if (data==TRUE){
+    agd_data <- DBI::dbReadTable(con, "data")
+    agd_data$dataTimestamp <- as.POSIXlt((agd_data$dataTimestamp / 1e7), origin = "0001-01-01 00:00:00", tz = "GMT")
+    agd_data$Date <- format(as.Date(agd_data$dataTimestamp), "%m/%d/%Y")
+    agd_data$Time <- format(agd_data$dataTimestamp, "%H:%M:%S")
+    agd_data$dataTimestamp <- NULL
+    namekey <- c(Date = "Date", Time = " Time", axis1 = " Axis1", axis2 = "Axis2", axis3 = "Axis3", steps = "Steps", 
+                 hr = "HR", lux = "Lux", inclineOff = "Inclinometer Off", inclineStanding = "Inclinometer Standing", 
+                 inclineSitting = "Inclinometer Sitting", inclineLying = "Inclinometer Lying")
+    names(agd_data) <- namekey[names(agd_data)]
+    agd_data <- cbind(agd_data[, tail(names(agd_data), 2)], agd_data[, 1:(ncol(agd_data)-2)])
+    agd_data$`Vector Magnitude` <- round((sqrt(agd_data$` Axis1`^2 + agd_data$Axis2^2 + agd_data$Axis3^2)), 2)
+  }
+  DBI::dbDisconnect(con)
+  if (settings==TRUE & data==TRUE) {
+    return(list(agd_set, agd_data))
+  } else if (settings==TRUE & data==FALSE) {
+    return(data.frame(agd_set))
+  } else if (settings==FALSE & data==TRUE) {
+    return(data.frame(agd_data))
+  }
+}
+
+
+#' @title Convert AGD to CSV Files
+#' @description Converts ActiGraph AGD to CSV Files
+#' @param data_directory Location of the folder where the ActiGraph
+#' @return Returns a CSV folder in the data directory with the ActiGraph CSV Files
+#' @details Converts ActiGraph AGD to CSV Files
+#' @examples 
+#' \dontrun{
+#' if(interactive()){
+#'  #EXAMPLE1
+#'  }
+#' }
+#' @seealso 
+#'  \code{\link[readr]{write_delim}}
+#' @rdname agd_to_csv
+#' @export 
+#' @importFrom readr write_csv
+agd_to_csv <- function(data_directory) {
+  start_time <- Sys.time()
+  setwd(data_directory)
+  new_data_directory <- paste0(data_directory, "/CSV")
+  if (dir.exists(new_data_directory)==FALSE) {
+    dir.create(new_data_directory)
+  }
+  files <- sort(list.files(data_directory))
+  agd_files <- grep(".agd", files, value=TRUE)
+  no.files <- length(list.files(new_data_directory))
+  for (file in agd_files) {
+    if (file.exists(paste0(new_data_directory, "/", gsub(".agd", ".csv", file)))==FALSE){
+      new_filename <- gsub(".agd", ".csv", file)
+      agd_settings <- read_agd(paste0(data_directory, "/", file), settings=TRUE, data=FALSE)
+      agd_data <- read_agd(paste0(data_directory, "/", file), settings=FALSE, data=TRUE)
+      readr::write_csv(agd_settings, paste0(new_data_directory, "/", new_filename), append = FALSE, col_names = FALSE)
+      readr::write_csv(agd_data, paste0(new_data_directory, "/", new_filename), append = TRUE, col_names = TRUE)
+    }
+  }
+  no.files <- length(list.files(new_data_directory)) - no.files
+  end_time <- Sys.time()
+  paste0("All done! This accelerometer program took ", round(((end_time - start_time)), 2), " seconds to convert ", no.files, " records")
+}
