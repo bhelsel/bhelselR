c123b5eab24211a913dcd60629c196aa6e2e44d8
Brian Helsel <bhelsel80@gmail.com>
2021-09-09 13:20:30 -0500

    Added default Vignette and simple package description file

--- R/bhelselR.R
+++ R/bhelselR.R
@@ -1,18 +1,19 @@
-#' bhelselR: A package for computating the notorious bar statistic
+#' @title bhelselR: A package for physical activity and weight management research.
 #'
-#' The bhelselR package provides several important functions:
+#' @description The bhelselR package provides several important functions:
 #' 
-#' @section Foo functions:
-#' agd_to_csv
-#' child_bmi_percentile
-#' dummy_2_categorical
-#' freqper
-#' gt3x2csv
-#' meansd
-#' percent_over_bmi_50
-#' read_agd
+#' @section bhelselR functions:
+#' * [bhelselR::readActigraph()]
+#' * child_bmi_percentile
+#' * dummy_2_categorical
+#' * freqper
+#' * gt3x2csv
+#' * meansd
+#' * percent_over_bmi_50
+#' * read_agd
 #'
 #' @docType package
 #' @name bhelselR
 NULL
 #> NULL
+
